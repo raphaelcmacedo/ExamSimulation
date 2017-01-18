ddb07707b514e47087a9414041edc21dd4ec1e81
raphaelcmacedo <raphaelcmacedo@hotmail.com>
2017-01-18 19:47:20 -0200

    Initial commit

---
+++ src/answers/Palindrome.java
@@ -0,0 +1,53 @@
+package answers;
+
+public class Palindrome {
+
+	//Performance: O(n)
+	public static boolean isPalindrome(String text){
+		
+		//I decided to separate validation and processing in order to achieve greater cohesion  
+		if(!isInputValid(text)){
+			return false;
+		}
+		
+		//I declare the first and last index of the string in order to compare them to each interaction, 
+		//so that the first index always increases and the last always decreases. 
+		//That way I can get each pair correctly 
+		int startIndex = 0;
+		int endIndex = text.length() - 1;
+		
+		while (startIndex < endIndex) {
+			//I separate the values obtained from this interaction and compare the chars. 
+			//It would be possible to make this comparison in less lines of code, 
+			//but I believe this way the code became more readable
+			char startChar = text.charAt(startIndex);
+			char endChar = text.charAt(endIndex);
+			if (startChar != endChar) {
+				return false;
+			}
+			//As stated above, I make the necessary increases and decrements
+			startIndex++;
+			endIndex--;
+		}
+		//If all the pairs of the string have been traversed without finding any difference, 
+		//the value entered is Palindrome
+		return true;
+	}
+	
+	//Validation method
+	public static boolean isInputValid(String text)
+	{
+		//Validates if the input is not null
+		if(text == null) {
+			return false;
+		}
+
+		//Validates if the input is not empty
+		if(text.isEmpty()){
+			return false;
+		}
+		
+		return true;
+	}
+
+}
---
+++ src/tests/PalindromeTest.java
@@ -0,0 +1,46 @@
+package tests;
+
+import static org.junit.Assert.*;
+import junit.framework.TestCase;
+import answers.Palindrome;
+import org.junit.Test;
+
+
+public class PalindromeTest extends TestCase {
+
+	@Test
+	public void testNull() {
+		boolean actual = Palindrome.isPalindrome(null);
+		assertEquals(false, actual);
+	}
+	
+	@Test
+	public void testEmpty() {
+		boolean actual = Palindrome.isPalindrome("");
+		assertEquals(false, actual);
+	}
+	
+	@Test
+	public void test1221() {
+		boolean actual = Palindrome.isPalindrome("1221");
+		assertEquals(true, actual);
+	}
+
+	@Test
+	public void test1121() {
+		boolean actual = Palindrome.isPalindrome("1121");
+		assertEquals(false, actual);
+	}
+	
+	@Test
+	public void testLetters() {
+		boolean actual = Palindrome.isPalindrome("abba");
+		assertEquals(true, actual);
+	}
+	
+	@Test
+	public void testOddChars() {
+		boolean actual = Palindrome.isPalindrome("abcba");
+		assertEquals(true, actual);
+	}
+}
